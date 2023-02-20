1cc9cb8611505dda7e5cd26e762ecaa8f2796fed
buchgit <buchgit@gmail.com>
2023-02-21 01:17:36 +0300

    c3

--- src/main/kotlin/com/example/messenger_kotlin/services/UserService.kt
+++ src/main/kotlin/com/example/messenger_kotlin/services/UserService.kt
@@ -1,4 +1,11 @@
 package com.example.messenger_kotlin.services
 
+import com.example.messenger_kotlin.models.User
+
 interface UserService {
+    fun attemptRegistration(userDetails: User):User
+    fun listUsers(currentUser: User):List<User>
+    fun retrieveUserData(userName:String):User?
+    fun retrieveUserData(id:Long):User?
+    fun userNameExists(userName:String):Boolean
 }
