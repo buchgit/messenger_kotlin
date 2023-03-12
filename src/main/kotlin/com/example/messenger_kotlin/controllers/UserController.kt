a2dfed3d7a588217e18b85b69a4b3e5cab9999a1
buchgit <buchgit@gmail.com>
2023-03-12 22:31:57 +0300

    UserController

--- src/main/kotlin/com/example/messenger_kotlin/controllers/UserController.kt
+++ src/main/kotlin/com/example/messenger_kotlin/controllers/UserController.kt
@@ -1,8 +1,10 @@
 package com.example.messenger_kotlin.controllers
 
+import com.example.messenger_kotlin.components.UserAssembler
 import com.example.messenger_kotlin.models.User
 import com.example.messenger_kotlin.repositories.UserRepository
 import com.example.messenger_kotlin.services.UserServiceImpl
+import com.example.messenger_kotlin.usecases.UserVO
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
@@ -12,11 +14,16 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/users")
-class UserController(val userServiceImpl: UserServiceImpl,userRepository: UserRepository) {
+class UserController(
+    val userServiceImpl: UserServiceImpl,
+    val userRepository: UserRepository,
+    val userAssembler: UserAssembler
+) {
     @PostMapping
     @RequestMapping("/registrations")
-    fun create(@Validated @RequestBody userModel: User):ResponseEntity<User>{
+    fun create(@Validated @RequestBody userModel: User): ResponseEntity<UserVO> {
         val user = userServiceImpl.attemptRegistration(userModel)
-        return ResponseEntity.ok(user)
+        val userVO = userAssembler.toUserVO(user)
+        return ResponseEntity.ok(userVO)
     }
 }
